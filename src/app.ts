4591ca4f10af13e72417c1088abf835304329a90
maurogarzia <maurogarzia2@gmail.com>
2025-11-25 17:02:56 -0300

    fix to routes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import app from "./server/server";
-import movementsRoutes from '../src/routes/MovementRoute'
-import usersRoutes from '../src/routes/UsersRoute'
-import authRoutes from '../src/routes/AuthRouter'
+import movementsRoutes from './routes/MovementRoute'
+import usersRoutes from './routes/UsersRoute'
+import authRoutes from './routes/AuthRouter'
 import mongoose from "mongoose";
 
 
