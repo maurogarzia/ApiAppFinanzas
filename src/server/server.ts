e4703e4136931a157ffa70c4c0e05e83000fa521
maurogarzia <maurogarzia2@gmail.com>
2025-11-26 15:14:42 -0300

    fix to cors in server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -8,7 +8,7 @@ const app : Application = express()
 dotenv.config()
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173", 
+    origin: [`${process.env.FRONTEND_URL}` , "http://localhost:5173"], 
     credentials: true, 
 }))
 app.use(express.json())
