7fa324b596a47f7203bfa1504cb30a265bd2da77
maurogarzia <maurogarzia2@gmail.com>
2026-01-22 11:33:37 -0300

    delete route from mobileAuth

--- src/routes/AuthRouter.ts
+++ src/routes/AuthRouter.ts
@@ -10,7 +10,4 @@ const router = Router()
 router.get('/google', passport.authenticate("google", {scope: ['profile', 'email']}))
 router.get('/google/callback', passport.authenticate("google", {session: false}), AuthController.googleCallback)
 
-// Ruta de google para mobile
-router.post('/google/mobile', AuthController.googleMobileLogin)
-
 export default router
